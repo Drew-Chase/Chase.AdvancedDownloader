82d3ecd798f1d3c419b486ce7dd2d22a91aa55bd
Drew Chase <dcmanproductions@gmail.com>
2023-03-11 15:29:32 -0500

    added download event

---
+++ Events/DownloadProgressUpdateEventArgs.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Chase.AdvancedDownloader.Events;
+
+public class DownloadProgressUpdateEventArgs : EventArgs
+{
+    public float Percentage { get; }
+    public long BytesRemaining { get; }
+    public long BytesDownloaded { get; }
+    public long TotalBytes { get; }
+    public long BytesPerSecond { get; }
+
+    internal DownloadProgressUpdateEventArgs(long bytesDownloaded, long totalBytes, long bytesPerSecond)
+    {
+        BytesDownloaded = bytesDownloaded;
+        TotalBytes = totalBytes;
+        BytesPerSecond = bytesPerSecond;
+        Percentage = bytesDownloaded / totalBytes;
+        BytesRemaining = bytesDownloaded - totalBytes;
+    }
+}
