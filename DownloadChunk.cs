76f6da669e64c2a01623ebeae908cc82c3fc5830
Drew Chase <dcmanproductions@gmail.com>
2023-03-11 15:29:22 -0500

    added struct

---
+++ DownloadChunk.cs
@@ -0,0 +1,10 @@
+﻿namespace Chase.AdvancedDownloader;
+
+internal struct DownloadChunk
+{
+    public long BytesDownloaded;
+    public long BytesPerSecond;
+    public long End;
+    public long Start;
+    public long Size => End - Start;
+}
